07cc45eebe5cdf2cd63e271653fcc8e89b1a7067
Omar <omarelgammal2004@gmail.com>
2022-10-13 15:59:34 +0200

    Added setter and getter functions and modified constructors

--- A1_P1_20210172-20210271-20210330/BDI_Lib.cpp
+++ A1_P1_20210172-20210271-20210330/BDI_Lib.cpp
@@ -8,20 +8,30 @@ class BigDecimalInt{
     private:
         string decStr;
         int decInt;
+        void setDecStr(string decStr_){
+            decStr = decStr_;
+        }
+        string getDecStr(){
+            return decStr;
+        }
     // Public functions body
     public:
         // Constructor functions to check the format of the entered number
-        BigDecimalInt(string decStr){
+        BigDecimalInt(string decString){
             string validNum = "^[+-]?[0-9]+$";
             regex correctNum(validNum);
-            if (!regex_match(decStr, correctNum)){
+            if (!regex_match(decString, correctNum)){
                 cerr << "INVALID FORMAT!!\n";
             }
+            decStr = decString;
         }
-        BigDecimalInt(int decInt){
+        BigDecimalInt(int decInt) {
             // Check for the max value for the "int" data type
-            if(decInt > 32767 || decInt < -32767){
+            if (decInt > 32767 || decInt < -32767) {
                 cerr << "INVALID INTEGER!!\n";
+            }
+            else {
+                decStr = to_string(decInt);
             }
         }
 };

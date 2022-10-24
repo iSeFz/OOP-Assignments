64d5d876bc46fc05deb9275a2b18945dab130460
Omar Elgammal <omarelgammal2004@gmail.com>
2022-10-24 19:29:55 +0200

    Added deep copying constructor in "SmartArray" class

--- 20210271/A1_SheetPb8_20210271.cpp
+++ 20210271/A1_SheetPb8_20210271.cpp
@@ -1,19 +1,3 @@
-// Assume that array class is modern C++ is not created yet. We want to create a smarter version of the
-// normal arrays that is aware of its size. Create this as a template class with the following features:
-// The smart array takes a type parameter to decide the type it will contain.
-// The smart array keeps track of its current size.
-// It has a destructor that frees any heap storage allocated by the smart array.
-// It has a method size ( ) that returns the number of elements in the array.
-// overriding the bracket-selection operator, which has the following prototype:
-// int & operator[] (int k) ;
-// Your implementation of operator [ ] should check to make sure that the index k is valid.
-// You should return the appropriate element in the internal array by reference so that clients can
-// assign a new value to a selection expression.
-// Overload the assignment operator to copy and array and implement deep copying. See page 551 for
-// details in Programming Abstractions book.
-// Implement a copy constructor that takes another array of the same type and creates an identical one
-// with copies of the values in the original array.
-// Write a program to test the array on different types.
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -24,90 +8,62 @@ private:
     int arraySize;
 public:
     SmartArray(int size){
-    array = new T[size];
-    arraySize = size;
-}
+        array = new T[size];
+        arraySize = size;
+    }
+    SmartArray(const SmartArray & otherArray){
+        arraySize = otherArray.arraySize;
+        array = new T[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = otherArray.array[i];
+        }
+    }
     ~SmartArray(){
-    delete[] array;
-}
+        delete[] array;
+    }
     int size(){
-    return arraySize;
-}
-    int & operator[](int k){
-    if (k >= arraySize) {
-        cout << "Error: Index out of range." << endl;
-        exit(1);
+        return arraySize;
     }
-    return array[k];
-}
+    T & operator[](int k){
+        if (k >= arraySize) {
+            cout << "Error: Index out of range." << endl;
+            exit(1);
+        }
+        return array[k];
+    }
     void append(T value){
-    T *newArray = new T[arraySize + 1];
-    for (int i = 0; i < arraySize; i++) {
-        newArray[i] = array[i];
+        T *newArray = new T[arraySize + 1];
+        for (int i = 0; i < arraySize; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[arraySize] = value;
+        delete[] array;
+        array = newArray;
+        arraySize++;
     }
-    newArray[arraySize] = value;
-    delete[] array;
-    array = newArray;
-    arraySize++;
-}
     void print(){
-    for (int i = 0; i < arraySize; i++) {
-        cout << array[i] << " ";
+        for (int i = 0; i < arraySize; i++){
+            cout << array[i] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
+
 };
 
-// SmartArray::SmartArray(int size) {
-//     array = new T[size];
-//     arraySize = size;
-// }
 
-// SmartArray::~SmartArray() {
-//     delete[] array;
-// }
-
-// int SmartArray::size() {
-//     return arraySize;
-// }
-
-// int & SmartArray::operator[](int k) {
-//     if (k >= arraySize) {
-//         cout << "Error: Index out of range." << endl;
-//         exit(1);
-//     }
-//     return array[k];
-// }
-
-// void SmartArray::append(T value) {
-//     T *newArray = new T[arraySize + 1];
-//     for (int i = 0; i < arraySize; i++) {
-//         newArray[i] = array[i];
-//     }
-//     newArray[arraySize] = value;
-//     delete[] array;
-//     array = newArray;
-//     arraySize++;
-// }
-
-// void SmartArray::print() {
-//     for (int i = 0; i < arraySize; i++) {
-//         cout << array[i] << " ";
-//     }
-//     cout << endl;
-// }
 
 int main()
 {
-    SmartArray<int> a(3); // create an array with 3 elements
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
-    a.append(4);
-    a.append(5);
-    a.append(6);
-    a.print(); // should print 1 2 3 4 5 6
-    a[6] = 7; // should print an error message and terminate
+    SmartArray<char> a(3); // create an array with 3 elements
+    a[0] = '1';
+    a[1] = '2';
+    a[2] = '3';
+    a.append('4');
+    a.append('5');
+    a.append('k');
+    SmartArray<char> b(a);
+    b.append('6');
+    b.print();
+    a.print();
     return 0;
 }
-

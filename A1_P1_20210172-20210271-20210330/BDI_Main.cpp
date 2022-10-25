30ae4d68989217b74f12aa2c850e7b91469ca645
SeifYahia <seifyahia@gmail.com>
2022-10-25 12:49:55 +0200

    Added sheet test cases & modified project version

--- A1_P1_20210172-20210271-20210330/BDI_Main.cpp
+++ A1_P1_20210172-20210271-20210330/BDI_Main.cpp
@@ -5,23 +5,23 @@ Team Members:
     Omar Elgammal - 20210271
     Mohamed Amgad - 20210330
 Program Name: Big Decimal Integer Program
-Last Modification Date: 14 October 2022
-Version: 3.0
+Last Modification Date: 25 October 2022
+Version: 4.0
 */
 
 #include "BDI_Header.hpp"
 
 int main(){
-    // Test cases:
+    // Sheet test cases:
     BigDecimalInt num1("123456789012345678901234567890");
     BigDecimalInt num2("+113456789011345678901134567890");
     BigDecimalInt num4 = num2 + num1;
-    // BigDecimalInt num5 = num2 - num1;
+    BigDecimalInt num5 = num2 - num1;
     cout << "num1 = " << num1 << endl;
     cout << "num2 = " << num2 << endl;
-    //Next statement will print 236913578023691357802369135780
+    // Next statement will print 236913578023691357802369135780
     cout << "num2 + num1 = " << num4 << endl;
-    //Next statement will print -313456789011345678901134567890
-    // cout << "num2 - num1 = " << num5 << endl;
+    // Next statement will print -10000000001000000000100000000
+    cout << "num2 - num1 = " << num5 << endl;
     return 0;
 }

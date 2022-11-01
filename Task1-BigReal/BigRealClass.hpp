147c6fea926c2fdc902e21406997aa3323b777d5
SeifYahia <seifyahia@gmail.com>
2022-11-01 23:30:22 +0200

    Added comments to the header file

--- Task1-BigReal/BigRealClass.hpp
+++ Task1-BigReal/BigRealClass.hpp
@@ -12,48 +12,53 @@ class BigReal{
     // Private section that contain data members and private functions
     private:
         BigDecimalInt modifiedReal;
+        // Store the point position to preserve it
         int pointPosition;
+        // Check if the input is valid or not
         bool isValidInput(string bigStr);
+        // Remove the decimal point before passing the number to the BigDecimalInt class
         void removeDecimalPoint(string & realStr);
+        // Return the decimal point to its original position after making operations
         string returnPoint();
-        BigDecimalInt & getModifiedReal();
     public:
+        // Getter function to get the modifiedReal value
+        BigDecimalInt & getModifiedReal();
         // Default constructor function that accept double as parameter
         BigReal(double real = 0.0);
         // Overload the constructor function to accept string as parameter
         BigReal(string real);
         // Overload the constructor function to accept BigDecimalInt objects as parameter
         BigReal(BigDecimalInt bigInteger);
-        // Copy constructor
+        // Copy constructor to copy objects into each other
         BigReal(const BigReal & other);
-        // Move constructor
+        // Move constructor to move object into another object
         BigReal(BigReal && other);
-        // Overload the assignment operator to assign values to realStr
+        // Overload the assignment operator to assign values to modifiedReal
         BigReal & operator = (BigReal & other);
-        // Move Assignment
+        // Move Assignment to move object into another object
         BigReal & operator = (BigReal && other);
-        // Overloading the plus operator to work with BigReal objects
+        // Overload the plus operator to work with BigReal objects
         BigReal operator + (BigReal & other);
-        // Overloading the minus operator to work with BigReal objects
+        // Overload the minus operator to work with BigReal objects
         BigReal operator - (BigReal & other);
-        // Overloading the less than "<" operator to work with BigReal objects
+        // Overload the less than "<" operator to work with BigReal objects
         bool operator < (BigReal & anotherReal);
-        // Overloading the greater than ">" operator to work with BigReal objects
+        // Overload the greater than ">" operator to work with BigReal objects
         bool operator > (BigReal & anotherReal);
-        // Overloading the equality "==" operator to check if the two numbers are equal
+        // Overload the equality "==" operator to check if the two numbers are equal
         bool operator == (BigReal & anotherReal);
-        // Function to return size of BigReal
+        // Return size of BigReal
         int size();
-        // Function to return sign of BigReal
+        // Return sign of BigReal
         int sign();
         // Declare friend functions to have access to all class data members
         friend ostream & operator << (ostream & out, BigReal & real);
         friend istream & operator >> (istream & in, BigReal & real);
 };
 
-// Overloading the exertion operator "<<" to print objects
+// Overload the exertion operator "<<" to print objects
 ostream & operator << (ostream & out, BigReal & real);
-// Overloading the insertion operator ">>" to take input
+// Overload the insertion operator ">>" to take input
 istream & operator >> (istream & in, BigReal & real);
 
 #endif

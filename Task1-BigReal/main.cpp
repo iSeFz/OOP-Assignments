0dd45066d5d8c296e16cccb389cb56deba4d3672
SeifYahia <seifyahia@gmail.com>
2022-11-06 22:40:52 +0200

    Updated the version of the project + sheet test case

--- Task1-BigReal/main.cpp
+++ Task1-BigReal/main.cpp
@@ -5,35 +5,39 @@ Team Members:
     Omar Elgammal - 20210271
     Mohamed Amgad - 20210330
 Program Name: Big Real Program
-Last Modification Date: 4 November 2022
-Version: 4.5
+Last Modification Date: 6 November 2022
+Version: 5.0
 */
 
 #include "BigRealClass.hpp"
 
-int main()
-{
-    // 5546.5466 + 6845.12 = 12,391.6666
-    BigReal a("00779.00990100");
-    BigReal b("00022.99009900");
-    BigReal c, d;
-    c = a + b;
-    d = a - b;
-    cout << a << " + " << b << " = " << c << endl;
-    cout << a << " - " << b << " = " << d << endl;
-
-    /*
+int main(){
     // Sheet test case
     BigReal n1 ("11.9000000000000000000000000000000001");
-    cout << "n1 = " << n1 << endl;
     BigReal n2 ("2333333333339.1134322222222292");
-    cout << "n2 = " << n2 << endl;
-    // Should print 2333333333351.013432222
-    // But it needs some SAD fixing
-    BigReal n3 = n1 + n2;
-    cout << "n1 + n2 = " << n3 << endl;
-    n3 = (n3 + BigReal (0.9));
-    cout << "n3 + 0.9 = " << endl;
+    BigReal n3 = n1 + n2; // 2333333333351.013432222
+    cout << n1 <<  " + " << n2 << " = " << n3 << endl;
+    n3 = n1 - n2; // −2333333333327.213432222
+    cout << n1 <<  " - " << n2 << " = " << n3 << endl;
+
+    // To Do : Copy and Move constructors
+    /*
+    // n3 = (n3 + BigReal (0.9));
+    // cout << "n3 + 0.9 = " << endl;
+    // Testing insertion operator
+    BigReal test2;
+    cin >> test2;
+    // Testing exertion operator
+    cout << test2;
     */
+
+    // Our test case
+    BigReal a("-005546.0066");
+    BigReal b("006845.1200");
+    BigReal c, d;
+    c = a + b; // 1299.1134
+    d = a - b; // -12391.1266
+    cout << a << " + " << b << " = " << c << endl;
+    cout << a << " - " << b << " = " << d << endl;
     return 0;
 }

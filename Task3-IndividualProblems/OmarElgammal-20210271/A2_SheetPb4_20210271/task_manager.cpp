ada0557c018242d96e8ff0cc6bf3f2f3db5c09fb
Omar Elgammal <omarelgammal2004@gmail.com>
2022-11-11 23:01:47 +0200

    Completed task manager with all its features

--- Task3-IndividualProblems/OmarElgammal-20210271/A2_SheetPb4_20210271/task_manager.cpp
+++ Task3-IndividualProblems/OmarElgammal-20210271/A2_SheetPb4_20210271/task_manager.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 class proccess{
@@ -32,20 +35,83 @@ class taskManager{
         tasks.push_back(p);
     }
     void print(){
-        cout << left << setw(20) << "Program Name";
+        cout << left << setw(35) << "Program Name";
             cout << right << setw(10)<< "PID";
             cout << right << setw(15) << "MemoryUsed"<< endl;
         for(auto i : tasks){
-            cout << left << setw(20) << i.getName();
+            cout << left << setw(35) << i.getName();
             cout << right << setw(10)<< i.getID();
             cout << right << setw(15) << i.getMemory() << "K" << endl;
+        }
+    }
+    void getCurrentTasks(){
+        system("tasklist > tasks.txt");
+        ifstream file("tasks.txt");
+        string line;
+        string trash;
+        // ignoring first 2 lines
+        getline(file, line);
+        getline(file, line);
+        getline(file, line);
+        // using stringstream to get each word
+        while(getline(file, line)){
+            string name, memory;
+            name = line.substr(0, 25);
+            while(name.back() == ' '){
+                name.pop_back();
+            }
+            line = line.substr(25, line.length() - 1);
+            stringstream ss(line);
+            int id;
+            ss >> id >> trash >> trash >> memory;
+            proccess p(name, id, memory);
+            addProccess(p);
+        }
+    }
+    void sort_by_memory(char order = 'd'){
+        if(order == 'a'){
+            sort(tasks.begin(), tasks.end(), [](proccess a, proccess b){
+            if(a.getMemory().size() < b.getMemory().size()){
+                return true;
+            }
+            else if(a.getMemory().size() == b.getMemory().size()){
+                return a.getMemory() < b.getMemory();
+            }
+            return false;
+        });
+        }
+        else{
+            sort(tasks.begin(), tasks.end(), [](proccess a, proccess b){
+            if(a.getMemory().size() > b.getMemory().size()){
+                return true;
+            }
+            else if(a.getMemory().size() == b.getMemory().size()){
+                return a.getMemory() > b.getMemory();
+            }
+            return false;
+        });
+        }
+        
+    }
+    void sort_by_id(){
+        sort(tasks.begin(), tasks.end(), [](proccess a, proccess b){
+            return a.getID() < b.getID();
+        });
     }
     
-}};
+    void sort_by_name(){
+        sort(tasks.begin(), tasks.end(), [](proccess a, proccess b){
+            return a.getName() < b.getName();
+        });
+    }
+};
 
 int main(){
     taskManager tm;
-    tm.addProccess(proccess("Chrome", 5, "10000000"));
+    tm.getCurrentTasks();
+    tm.sort_by_memory('a');
+    // tm.sort_by_name();
+    // tm.sort_by_id();
     tm.print();
 
 }

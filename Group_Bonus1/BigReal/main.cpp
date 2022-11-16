b0c1c92056d88bea628d0d9bfcd525f39ceb6342
Mohamed Amgad <mohamedamgad233@gmail.com>
2022-11-16 16:53:59 +0200

    Added Group_Bonus1 folder

---
+++ Group_Bonus1/BigReal/main.cpp
@@ -0,0 +1,16 @@
+/*
+FCAI - OOP - 2022 - Assignment #2
+Team Members:
+    Seif Yahia    - 20210172
+    Omar Elgammal - 20210271
+    Mohamed Amgad - 20210330
+Program Name: Big Real Program
+Last Modification Date: 6 November 2022
+Version: 5.0
+*/
+
+
+int main(){
+
+    return 0;
+}

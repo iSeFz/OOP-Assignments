4a1941370050da2512853ccc7014abecb6139d73
SeifYahia <seifyahia@gmail.com>
2022-12-22 18:36:13 +0200

    Updated the version of the file + test cases

--- Task#1/main.cpp
+++ Task#1/main.cpp
@@ -4,38 +4,59 @@ Team Members:
     Seif Yahia    - 20210172
     Mohamed Amgad - 20210330
 Program Name: MSVector
-Last Modification Date: 11 December 2022
-Version: 1.5
+Last Modification Date: 22 December 2022
+Version: 2.0
 */
 
 #include "msVector.hpp"
+#include <unistd.h>
 
 int main(){
-    MSVector <string> v1(4);
-    v1.push_back((string) "seif");
-    v1.push_back((string) "lol");
-    v1.push_back((string) "amgad");
-    v1.push_back((string) "waw");
+    MSVector<int> nums1;
+    MSVector<int> nums2;
+    nums1.push_back(5);
+    nums1.push_back(9);
+    nums1.push_back(8);
+    cout << "nums1 contents:\n" << nums1;
+    cout << "nums1[2] = " << nums1[2] << endl;
+    cout << "Deleted item: " << nums1.pop_back() << endl;
+    cout << "nums1 contents:\n" << nums1;
+    nums2 = nums1;
+    cout << "Copied nums1 into nums2\n";
+    cout << "nums2 contents:\n" << nums2;
+    nums2.push_back(19);
+    cout << "Added 19 to nums2\n";
+    cout << "nums1 contents:\n" << nums1;
+    cout << "nums2 contents:\n" << nums2;
+    MSVector<int> nums3 = nums2;
+    cout << "nums3 contents:\n" << nums3;
+    nums1.Clear();
+    cout << "Cleared nums1\n";
+    cout << "nums1 contents:\n" << nums1;
+
+    cout << "Wait for testing another data type...\n\n";
+    sleep(3);
+
+    MSVector <string> v1;
+    v1.push_back((string) "Vector");
+    v1.push_back((string) "C++");
+    v1.push_back((string) "OOP");
+    v1.push_back((string) "Programming");
+    cout << v1;
     cout << "Capcity before adding: " << v1.Capacity() << endl; // 4
     v1.push_back((string) "testing");
     cout << "Capcity after adding: " << v1.Capacity() << endl; // 8
     cout << "Size after adding: " << v1.Size() << endl; // 5
-    cout << "v1 contents: " << v1; // seif lol amgad waw testing
-    v1[3] = (string) "tryhard";
-    cout << "v1 contents: " << v1; // seif lol amgad tryhard testing
-    MSVector<string> v2 = v1;
-    cout << "v2 contents: " << v2; // seif lol amgad tryhard testing
-    cout  << std::boolalpha << "Result of equality operator: " << (v1 == v2) << endl; // true
-    v2[2] = "Salam Alykum231";
-    cout << "v2 contents: " << v2; // seif lol Salam Alykum231 tryhard testing
-    cout  << std::boolalpha << "Result of less than operator: " << (v1 < v2) << endl; // true
-    v2.push_back("new");
-    cout << "Size after adding: " << v2.Size() << endl; // 6
-    cout << "v1 contents: " << v1; // seif lol amgad tryhard testing
-    cout << "v2 contents: " << v2; // seif lol Salam Alykum231 tryhard testing new
-
-    cout << "Clearing v2\n";
-    v1.Clear();
-    cout << "Current Size: " << v1.Size() << endl;
-    cout << "v1 contents: " << v1 << endl;
+    v1.erase(v1.begin() + 1, v1.begin() + 3);
+    cout << v1;
+    cout << "Size after erasing from 1 to 3 = " << v1.Size() << endl; // 3
+    v1.insert(v1.end(),"insertedAtEnd");
+    v1.insert(v1.begin(),"insertedAtBegin");
+    cout << v1;
+    v1.erase(v1.begin());
+    v1.push_back("FinalTest");
+    cout << v1;
+    cout << "Capcity after adding: " << v1.Capacity() << endl; // 8
+    cout << "Size after adding: " << v1.Size() << endl; // 5
+    return 0;
 }

2b7396d09996c69f9e95bf3c09fc2fa5e4d6c754
SeifYahia <seifyahia@gmail.com>
2022-12-22 22:23:05 +0200

    Added "manageStack" function + some modifications

--- Task#4_Individual-Bonus/SeifYahia-20210172/A3_BonusPb8_20210172/stackWithExceptions.cpp
+++ Task#4_Individual-Bonus/SeifYahia-20210172/A3_BonusPb8_20210172/stackWithExceptions.cpp
@@ -2,7 +2,7 @@
 FCAI - OOP - 2022 - Assignment #3
 Author: Seif Yahia - 20210172
 Program Name: Individual Bonus - Problem 8 - Exceptions/Templates
-Last Modification Date: 16 December 2022
+Last Modification Date: 22 December 2022
 */
 
 #include <iostream>
@@ -23,6 +23,7 @@ public:
     Stack(const Stack<T>&);
     Stack<T>& operator=(const Stack<T>&);
     ~Stack();
+    void setSize(int);
     void push(T);
     T pop();
     T top();
@@ -67,44 +68,39 @@ Stack<T>& Stack<T> :: operator=(const Stack<T>& other){
 // Destructor to avoid memory leak
 template <typename T>
 Stack<T> :: ~Stack(){
-    size = 0;
+    topEl = 0;
     delete[] data;
 }
+
+// Set the size of the stack according to the user's input
+template <typename T>
+void Stack<T> :: setSize(int userSize){ size = userSize; }
 
 // Push an element at top of the Stack
 template <typename T>
 void Stack<T> :: push(T item){
-    try{
-        if(topEl == size){
-            throw StackOverflowException();
-        }
-        else{
-            data[topEl++] = item;
-        }
-    } catch(StackOverflowException){
-        cerr << "Stack is already full! Cannot push any element!\n";
+    if(topEl >= size){
+        throw StackOverflowException();
     }
+    data[topEl++] = item;
 }
 
 // Pop an element from the top of the Stack
 template <typename T>
 T Stack<T> :: pop(){
-    try{
-        if(this->empty()){
-            throw StackEmptyException();
-        }
-        else{
-            topEl--;
-        }
-    } catch(StackEmptyException){
-        cerr << "Stack is already empty! Cannot pop any element!\n";
-    }
-    return data[topEl];
+    T lastElement = this->top();
+    topEl--;
+    return lastElement;
 }
 
 // Return the element at the top of the Stack
 template <typename T>
-T Stack<T> :: top(){ return data[topEl - 1]; }
+T Stack<T> :: top(){
+    if(this->empty()){
+        throw StackEmptyException();
+    }
+    return data[topEl - 1];
+}
 
 // Return true if the Stack has no elements
 template <typename T>
@@ -116,48 +112,77 @@ bool Stack<T> :: empty(){
 // Print the current contents of the Stack
 template <typename T>
 void Stack<T> :: printStack(){
-    if(this->empty()) return;
+    if(this->empty()) {
+        cerr << "\n\tStack is EMPTY!!\n";
+        return;
+    }
+    cout << "\n";
     for(int i = topEl - 1; i >= 0; --i){
         cout << data[i] << "\n";
     }
 }
 
+// Function to manage the stack functions
+template <typename T>
+void manageStack(Stack<T> clientStack){
+    short choice;
+    int stackSize = 0;
+    string elementToPush;
+    while(true){
+        cout << "\nChoose one of the following options:\n";
+        cout << "1. Create a Stack\n"
+                "2. Check top element\n"
+                "3. Push an element\n"
+                "4. Pop an element\n"
+                "5. Print stack elements\n"
+                "6. Exit\n";
+        cout << "Your choice ==> ";
+        cin >> choice;
+        try{
+            if(choice == 1){
+                if(stackSize != 0){
+                    cerr << "\n\tYou have already created a stack!\n";
+                    continue;
+                }
+                cout << "Creating a stack for you...\n";
+                cout << "Enter the size of the stack ==> ";
+                cin >> stackSize;
+                clientStack.setSize(stackSize);
+                cout << "\n\tCreated a stack of size " << stackSize << " successfully!\n";
+            } else if(choice == 2){
+                string temp = clientStack.top();
+                cout << "\n\tCurrent top element is ==> " << temp << "\n";
+            } else if(choice == 3){
+                if(stackSize == 0){
+                    cerr << "\n\tYou should create a stack first!\n";
+                    continue;
+                }
+                cout << "Enter the item to push to the stack ==> ";
+                cin.ignore();
+                getline(cin, elementToPush);
+                clientStack.push(elementToPush);
+                cout << "\n\tPushed \"" << elementToPush << "\" to the stack.\n";
+            } else if(choice == 4){
+                string temp = clientStack.pop();
+                cout << "\n\tPopped \"" << temp << "\" from the stack.\n";
+            } else if(choice == 5){
+                clientStack.printStack();
+            } else if(choice == 6){ break; }
+            else{
+                cerr << "\n\tINVALID Choice! Choose ONLY from 1 to 6\n";
+            }
+        } catch(StackEmptyException){
+            cerr << "\n\tStack is already EMPTY!!\n";
+        } catch(StackOverflowException){
+            cerr << "\n\tStack is already FULL!!\n";
+        }
+    }
+    cout << "\n\tThank you for using FCAI Stack :)\n";
+}
+
 // Driver program to test the above class
 int main(){
-    Stack <string> s1;
-    s1.push("hello world");
-    s1.push("I'm learning C++ programming");
-    cout << "\ts1 contents:\n"; s1.printStack();
-    cout << "==============================\n";
-    // Testing copy constructor
-    Stack <string> s2 = s1;
-    cout << "Copying s1 into s2 via copy constructor...\n";
-    cout << "Pushing an element into s2...\n";
-    s2.push("new text in s2");
-    cout << "\ts1 contents:\n"; s1.printStack();
-    cout << "\ts2 contents:\n"; s2.printStack();
-    cout << "==============================\n";
-    // Testing copy assignment operator
-    Stack <string> s3;
-    s3 = s2;
-    cout << "Copying s1 into s2 via copy assignment operator...\n";
-    cout << "\ts3 contents:\n"; s3.printStack();
-    cout << "Popping from s3... " << s3.pop() << "\n";
-    cout << "\ts2 contents:\n"; s2.printStack();
-    cout << "\ts3 contents:\n"; s3.printStack();
-    cout << "==============================\n";
-    // Testing other functions of the Stack
-    Stack <int> s4(3);
-    s4.push(8);
-    s4.push(4);
-    s4.push(2);
-    cout << "\ts4 contents:\n"; s4.printStack();
-    s4.push(3);
-    cout << "Popping from s4... " << s4.pop() << "\n";
-    cout << "Top element now is ==> " << s4.top() << "\n";
-    cout << "Popping from s4... " << s4.pop() << "\n";
-    cout << "Popping from s4... " << s4.pop() << "\n";
-    s4.pop();
-    cout << "\tThank you for using FCAI Stack :)\n";
+    Stack <string> test;
+    manageStack(test);
     return 0;
 }
